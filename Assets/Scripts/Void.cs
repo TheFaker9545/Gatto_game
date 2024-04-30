34c1cf542029acf1a4f9608b9f6665d2c2481ea0
Alefalso <fammy594@gmail.com>
2024-05-01 01:45:25 +0200

    aggiunta cose
    è stato aggiunto un nemico che spara con dei proiettili che fanno danno al giocatore,
    un game over se si cade giù dallo stage

---
+++ Assets/Scripts/Void.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Void : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Player")){
+            HealthManager.health = 0;
+            if(HealthManager.health<=0){
+                PlayerManager.isGameOver = true;
+                player.gameObject.SetActive(false);
+            }
+        }
+    }
+}
